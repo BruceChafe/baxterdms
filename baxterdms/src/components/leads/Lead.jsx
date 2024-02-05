bece20017afbeb8d0ef1364ede78ed2ab4ac5038
BruceChafe <129335587+BruceChafe@users.noreply.github.com>
2024-02-05 19:30:37 -0330

    GG

--- baxterdms/src/components/leads/Lead.jsx
+++ baxterdms/src/components/leads/Lead.jsx
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Typography } from "@mui/material";
+import { Typography, Box, Tab, TextField } from "@mui/material";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
 
 const LeadComponent = () => {
+  const [value, setValue] = React.useState("1");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   const [lead, setLead] = useState(null);
   const { leadId } = useParams();
 
@@ -22,16 +29,107 @@ const LeadComponent = () => {
   };
 
   return (
-    <div>
-      {lead ? (
-        <Typography>
-          Lead Details for leadId: {leadId}
-          {lead.leadDealership}
-        </Typography>
-      ) : (
-        <Typography>Loading...</Typography>
+    <Box sx={{ p: 2, width: "100%", typography: "body1" }}>
+      {lead && (
+        <TabContext value={value}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider", mb: 2 }}>
+            <TabList onChange={handleChange} centered variant="fullWidth">
+              <Tab label="firstName lastName" disabled />
+              <Tab label="Lead Information" value="1" />
+              <Tab label="Lead Contact" value="2" />
+              <Tab label="Lead Vehicle" value="3" disabled />
+              <Tab label="Lead Trade-In" value="4" disabled />
+            </TabList>
+          </Box>
+          <Box sx={{ borderBottom: 1, borderColor: "divider", pl:3 }}>
+            <Typography variant="h6" sx={{mb:10}}>Status</Typography>
+          </Box>
+          <TabPanel value="1" sx={{ borderBottom: 1, borderColor: "divider"}}>
+            <>
+              <Typography variant="h6" sx={{ mb: 2 }}>
+                Lead Information
+              </Typography>
+              <TextField
+                variant="outlined"
+                label="Dealership"
+                value={lead.leadDealership}
+                disabled
+                sx={{ width: "45%", mr: 2, mb: 2 }}
+              />
+              <TextField
+                variant="outlined"
+                label="Status"
+                value={lead.leadStatus}
+                disabled
+                sx={{ width: "45%", mr: 2, mb: 2 }}
+              />
+              <br />
+              <TextField
+                variant="outlined"
+                label="Lead Source"
+                value={lead.leadSource}
+                disabled
+                sx={{ width: "45%", mr: 2, mb: 2 }}
+              />
+              <TextField
+                variant="outlined"
+                label="BDC Agent"
+                value={lead.leadBDCAgent}
+                disabled
+                sx={{ width: "45%", mr: 2, mb: 2 }}
+              />
+              <br />
+              <TextField
+                variant="outlined"
+                label="Lead Type"
+                value={lead.leadType}
+                disabled
+                sx={{ width: "45%", mr: 2, mb: 2 }}
+              />
+              <TextField
+                variant="outlined"
+                label="Sales Consultant"
+                value={lead.leadSalesConsultant}
+                disabled
+                sx={{ width: "45%", mr: 2, mb: 2 }}
+              />
+              <br />
+              <TextField
+                variant="outlined"
+                label="Vehicle of Interest"
+                value={lead.leadVehicle}
+                disabled
+                sx={{ width: "45%", mr: 2, mb: 2 }}
+              />
+              <TextField
+                variant="outlined"
+                label="Sales Manager"
+                value={lead.leadSalesManager}
+                disabled
+                sx={{ width: "45%", mr: 2, mb: 2 }}
+              />
+              <br />
+              <TextField
+                variant="outlined"
+                label="Trade-In"
+                value={lead.leadTradeVehicle}
+                disabled
+                sx={{ width: "45%", mr: 2, mb: 2 }}
+              />
+              <TextField
+                variant="outlined"
+                label="Finance Manager"
+                value={lead.leadFinanceManager}
+                disabled
+                sx={{ width: "45%", mr: 2, mb: 2 }}
+              />
+            </>
+          </TabPanel>
+          <TabPanel value="2">Item Two</TabPanel>
+          <TabPanel value="3">Item Three</TabPanel>
+        </TabContext>
       )}
-    </div>
+    </Box>
   );
 };
 
