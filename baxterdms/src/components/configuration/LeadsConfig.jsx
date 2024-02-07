e78aefc052e447a8ae4e93a0205885ebdd8acf61
BruceChafe <129335587+BruceChafe@users.noreply.github.com>
2024-02-07 19:11:26 -0330

    gg

--- baxterdms/src/components/configuration/LeadsConfig.jsx
+++ baxterdms/src/components/configuration/LeadsConfig.jsx
@@ -1,223 +1,217 @@
 import React, { useState, useEffect } from "react";
 import {
-  Typography,
-  TextField,
-  Divider,
-  Box,
-  Button,
   Grid,
-  MenuItem
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Checkbox,
+  Button,
+  Paper,
+  TextField,
+  Typography,
 } from "@mui/material";
-import { Navigate } from 'react-router-dom'
+import { Box } from "@mui/system";
 
-const NewLeadFields = [
-  { label: "Dealership", key: "leadDealership" },
-  { label: "Sales Consultant", key: "leadSalesConsultant" },
-  { label: "Source", key: "leadSource" },
-  { label: "Type", key: "leadType" },
-  { label: "Status", key: "leadStatus" },
-];
+function not(a, b) {
+  return a.filter((value) => b.indexOf(value) === -1);
+}
 
-const NewLeadForm = ({ onCloseForm, contactId }) => {
-  const [formData, setFormData] = useState({});
-  const [filledFields, setFilledFields] = useState({});
-  const [message, setMessage] = useState("");
-  const [formSubmitted, setFormSubmitted] = useState(false);
-  const [sourceOptions, setSourceOptions] = useState([]);
-  const [typeOptions, setTypeOptions] = useState([]);
+const LeadsConfig = () => {
+  const [checked, setChecked] = useState([]);
+  const [left, setLeft] = useState([]);
+  const [right, setRight] = useState([]);
+  const [newItem, setNewItem] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:8000/configLeads/1")
       .then((response) => response.json())
-      .then((configData) => {
-        const sourceOptions = configData.leadSourceActive || [];
-        const typeOptions = configData.leadTypeActive || [];
-
-        setSourceOptions(sourceOptions);
-        setTypeOptions(typeOptions);
-
+      .then((data) => {
+        setLeft(data.leadSourceUnactive || []);
+        setRight(data.leadSourceActive || []);
       })
       .catch((error) => {
-        console.error("Error fetching options:", error);
+        console.error("Error fetching configuration:", error);
       });
   }, []);
 
-  const handleInputChange = (key, value) => {
-    setFormData((prevData) => ({ ...prevData, [key]: value }));
-    setFilledFields((prevFilled) => ({ ...prevFilled, [key]: !!value }));
+  const leftChecked = not(checked, right);
+  const rightChecked = not(checked, left);
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
   };
 
-  const handleCreate = () => {
-    setFormSubmitted(true);
-
-    const allMandatoryFieldsFilled = NewLeadFields.every(
-      (field) => !!formData[field.key]
-    );
-
-    if (allMandatoryFieldsFilled) {
-      const newLeadNumber = Math.floor(Math.random() * 1000) + 1000;
-
-      fetch(`http://localhost:8000/Contacts/${contactId}`)
-        .then((response) => response.json())
-        .then((existingContact) => {
-          const existingLeadNumbers = existingContact.leadNumbers || [];
-
-          return fetch(`http://localhost:8000/Contacts/${contactId}`, {
-            method: "PATCH",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              leadNumbers: [...existingLeadNumbers, newLeadNumber],
-            }),
-          });
-        })
-        .then((response) => response.json())
-        .then((updatedContact) => {
-          const returnedLeadNumbers = updatedContact.leadNumbers || [];
-
-          console.log("Returned Lead Numbers:", returnedLeadNumbers);
-
-          const contactId = updatedContact.dmsID;
-
-          return fetch("http://localhost:8000/leads", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              leadNumber: newLeadNumber,
-              dmsID: contactId,
-              ...formData,
-            }),
-          });
-        })
-        .then((response) => response.json())
-        .then((newLead) => {
-          setMessage("Lead created successfully!");
-          setFormData({});
-          setFilledFields([]);
-  
-          console.log("New Lead:", newLead);
-  
-          return <Navigate to={`/leads/lead/${newLead.id}`} />;
-        })
-        .catch((error) => {
-          console.error("Error creating lead:", error);
-          setMessage("Error creating lead.");
-        });
-      } else {
-        setMessage("Please fill out all mandatory fields.");
-      }
-    };
-
-  const handleKeyPress = (event) => {
-    if (event.key === "Enter") {
-      handleCreate();
-    }
+  const handleAllRight = () => {
+    setRight(right.concat(left));
+    setLeft([]);
   };
 
-  const renderTextField = (label, key) => {
-    const isMandatory = NewLeadFields.find((field) => field.key === key);
-    const isFilled = filledFields[key];
-    const showError = formSubmitted && isMandatory && !isFilled;
-  
-    if (key === "leadSource") {
-      return (
-        <TextField
-          select
-          variant="outlined"
-          label={label}
-          value={formData[key] || ""}
-          onChange={(e) => handleInputChange(key, e.target.value)}
-          onKeyPress={handleKeyPress}
-          sx={{ mb: 2, width: "100%" }}
-          required={!!isMandatory}
-          error={showError}
-          helperText={showError ? "This field is required." : ""}
-        >
-          {sourceOptions.map((option) => (
-            <MenuItem key={option} value={option}>
-              {option}
-            </MenuItem>
-          ))}
-        </TextField>
-      );
-    }
-  
-    if (key === "leadType") {
-      return (
-        <TextField
-          select
-          variant="outlined"
-          label={label}
-          value={formData[key] || ""}
-          onChange={(e) => handleInputChange(key, e.target.value)}
-          onKeyPress={handleKeyPress}
-          sx={{ mb: 2, width: "100%" }}
-          required={!!isMandatory}
-          error={showError}
-          helperText={showError ? "This field is required." : ""}
-          key={key} // Add key prop with a unique value
-        >
-          {typeOptions.map((option) => (
-            <MenuItem key={option} value={option}>
-              {option}
-            </MenuItem>
-          ))}
-        </TextField>
-      );
-    }
-  
-    return (
-      <TextField
-        variant="outlined"
-        label={label}
-        value={formData[key] || ""}
-        onChange={(e) => handleInputChange(key, e.target.value)}
-        onKeyPress={handleKeyPress}
-        sx={{ mb: 2, width: "100%" }}
-        required={!!isMandatory}
-        error={showError}
-        helperText={showError ? "This field is required." : ""}
-      />
-    );
+  const handleCheckedRight = () => {
+    setRight(right.concat(leftChecked));
+    setLeft(not(left, leftChecked));
+    setChecked(not(checked, leftChecked));
   };
-  
 
-  const renderSection = (sectionLabel, fields) => (
-    <Box sx={{ mb: 3 }}>
-      <Typography variant="h6" sx={{ mb: 2 }}>
-        {sectionLabel}
-      </Typography>
-      <Grid container spacing={2}>
-        {fields.map((field) => (
-          <Grid item xs={12} key={field.label}>
-            {renderTextField(field.label, field.key)}
-          </Grid>
-        ))}
-      </Grid>
-      <Divider sx={{ mt: 2, mb: 2 }} />
-    </Box>
+  const handleCheckedLeft = () => {
+    setLeft(left.concat(rightChecked));
+    setRight(not(right, rightChecked));
+    setChecked(not(checked, rightChecked));
+  };
+
+  const handleAllLeft = () => {
+    setLeft(left.concat(right));
+    setRight([]);
+  };
+
+  const handleAddNewItem = () => {
+    setLeft([...left, newItem]);
+    setNewItem("");
+  };
+
+  const handleSave = () => {
+    fetch("http://localhost:8000/configLeads/1", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        leadSourceUnactive: left,
+        leadSourceActive: right,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Update successful:", data);
+      })
+      .catch((error) => {
+        console.error("Error updating configuration:", error);
+      });
+  };
+
+  const customList = (items) => (
+    <List dense component="div" role="list">
+      {items.map((value) => {
+        const labelId = `transfer-list-item-${value}-label`;
+        return (
+          <ListItemButton
+            key={value}
+            role="listitem"
+            onClick={handleToggle(value)}
+          >
+            <ListItemIcon>
+              <Checkbox
+                checked={checked.indexOf(value) !== -1}
+                tabIndex={-1}
+                disableRipple
+                inputProps={{
+                  "aria-labelledby": labelId,
+                }}
+              />
+            </ListItemIcon>
+            <ListItemText id={labelId} primary={value} />
+          </ListItemButton>
+        );
+      })}
+    </List>
   );
 
   return (
     <Box sx={{ p: 3, maxWidth: 600 }}>
       <Typography variant="h5" sx={{ mb: 3 }}>
-        New Lead Form
+        New Lead Configuration
       </Typography>
-      {renderSection("Basic Information", NewLeadFields)}
-      <Button variant="outlined" onClick={onCloseForm} sx={{ mb: 2 }}>
-        Back
-      </Button>
-      <Button variant="outlined" onClick={handleCreate} sx={{ mb: 2, ml: 2 }}>
-        Create
-      </Button>
-      <Typography variant="body1" color="error" sx={{ mt: 1, mb: 2 }}>
-        {message}
+      <Typography variant="h6" sx={{ mb: 3 }}>
+        Source
       </Typography>
+      <TextField
+          label="New Item"
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+          variant="outlined"
+          sx={{ mb: 2 }}
+        />
+        <Button
+          sx={{ my: 0.5 }}
+          variant="outlined"
+          size="small"
+          onClick={handleAddNewItem}
+          disabled={!newItem}
+          aria-label="add new item"
+        >
+          Add
+        </Button>
+      <Grid container justifyContent="space-between">
+        <Grid item>
+          <Paper variant="outlined" sx={{ height: "100%", minWidth: '200px'}}>
+            {customList(left)}
+          </Paper>
+        </Grid>
+        <Grid item sx={{ pt: 3 }}>
+          <Grid container direction="column">
+            <Button
+              variant="outlined"
+              size="small"
+              onClick={handleAllRight}
+              disabled={left.length === 0}
+              aria-label="move all right"
+            >
+              ≫
+            </Button>
+            <Button
+              variant="outlined"
+              size="small"
+              onClick={handleCheckedRight}
+              disabled={leftChecked.length === 0}
+              aria-label="move selected right"
+            >
+              &gt;
+            </Button>
+            <Button
+              variant="outlined"
+              size="small"
+              onClick={handleCheckedLeft}
+              disabled={rightChecked.length === 0}
+              aria-label="move selected left"
+            >
+              &lt;
+            </Button>
+            <Button
+              variant="outlined"
+              size="small"
+              onClick={handleAllLeft}
+              disabled={right.length === 0}
+              aria-label="move all left"
+            >
+              ≪
+            </Button>
+            <Button
+              variant="outlined"
+              size="small"
+              onClick={handleSave}
+              aria-label="save"
+            >
+              Save
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid item>
+        <Paper variant="outlined" sx={{ height: "100%", minWidth: '200px'}}>
+          <Grid item>{customList(right)}</Grid>
+        </Paper>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
 
-export default NewLeadForm;
+export default LeadsConfig;
