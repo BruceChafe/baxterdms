43ae27143f48e5507b10ee092447266ab0f86eb6
BruceChafe <129335587+BruceChafe@users.noreply.github.com>
2024-05-28 19:23:27 -0230

    vercel

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
-  },
-  base: '/'
+    outDir: 'dist',
+    rollupOptions: {
+      input: '/index.html'
+    }
+  }
 });
-
