be58e3cb06864eab5278a19d2cd5f3b638a1696b
BruceChafe <129335587+BruceChafe@users.noreply.github.com>
2024-06-03 18:03:33 -0230

    vercel

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
@@ -16,5 +18,8 @@ export default defineConfig({
         assetFileNames: 'assets/[name]-[hash].[ext]'
       }
     }
+  },
+  server: {
+    historyApiFallback: true,
   },
 });
